88d481643484c360e1bb44dddb034d3d4e45242b
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2021-01-05 12:07:28 -0600

    eff

--- R/globals.R
+++ R/globals.R
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 utils::globalVariables(c("test", "p_val", "Fstat", ".", "study", "var.eff.size"))
-=======
-utils::globalVariables(c("test", "p_val", "Fstat", ".", "study", "var.effect.size"))
->>>>>>> e08bc45bf6de889f4dd55fdc00de96e0145dfae9
