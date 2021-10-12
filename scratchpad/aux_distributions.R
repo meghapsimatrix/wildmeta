2a1bc1d5c5eefccb7ce8771f818c7001689a35b3
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2021-10-12 13:52:31 -0500

    aux distributions

--- scratchpad/aux_distributions.R
+++ scratchpad/aux_distributions.R
@@ -2,8 +2,10 @@ if(auxiliary_dist == "Rademacher"){
 
   wts <- sample(c(-1, 1), size = length(num_cluster), replace = TRUE)
 
-} else if(auxiliary_dist == "Maamen"){
+} else if(auxiliary_dist == "Mammen"){
 
-
+ wts <- sample(c(- (sqrt(5) - 1)/2,  (sqrt(5) + 1)/2), size = length(num_cluster), replace = TRUE,
+               prob = c((sqrt(5) + 1) /(2 * sqrt(5)), (sqrt(5) - 1) /(2 * sqrt(5)) ))
 
 }
+
