c832f4acebab84bd84fe001e4c53b2a12cf4789c
jepusto <jepusto@gmail.com>
2021-10-15 15:51:32 -0500

    Working on computing null X matrix.

--- scratchpad/metafor_scratchpad.R
+++ scratchpad/metafor_scratchpad.R
@@ -4,10 +4,25 @@ library(metafor)
 library(tidyverse)
 
 
-full_model <- rma.mv(yi = d ~ study_type,
+full_model <- rma.mv(yi = d ~ 0 + study_type + hrs + test,
                        V = V,
                        random = ~ study_type| study,
                        data = SATcoaching)
+summary(full_model)
+
+X <- full_model$X
+Cmat <- constrain_equal(1:3, coefs = coef(full_model))
+
+q <- nrow(Cmat)
+p <- ncol(Cmat)
+XtX_inv <- chol2inv(chol(crossprod(X)))
+Cnull <- diag(nrow = p) - XtX_inv %*% t(Cmat) %*% chol2inv(chol(Cmat %*% XtX_inv %*% t(Cmat))) %*% Cmat
+qr(X %*% Cnull)
+Xnull <- qr.X(qr(X %*% Cnull), ncol = p - q)
+
+null_model <- update(full_model, yi = full_model$yi, mods = ~ 0 + Xnull)
+null_model
+
 
 indices <- 2
 
@@ -25,6 +40,8 @@ study <- tibble::tibble(study = clubSandwich:::findCluster.rma.mv(full_model))
 
 dat <- dplyr::bind_cols(y_dat, x_dat, study)
 dat$study <- as.character(dat$study)
+
+
 
 null_formula <- paste(rownames(full_model$beta)[ - indices], collapse = " + ")
 null_formula <- stringr::str_replace(null_formula, "intrcpt", "1")
@@ -45,6 +62,9 @@ null_model <- metafor::rma.mv(yi = stats::as.formula(paste("effect_size ~ ", nul
 null_model <- update(full_model,
                      yi = stats::as.formula(paste("effect_size ~ ", null_formula)))  # but what if there are other things in the formula
 
+
+null_model <- update(full_model,
+                     yi = stats::as.formula(paste("effect_size ~ ", null_formula)))  # but what if there are other things in the formula
 null_model
 
 
