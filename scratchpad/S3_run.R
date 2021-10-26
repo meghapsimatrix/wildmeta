80ac835002a76b21af3c087e3b248c577dfa7fc5
Megha Joshi <33493716+meghapsimatrix@users.noreply.github.com>
2021-10-26 16:12:34 -0500

    not sure

---
+++ scratchpad/S3_run.R
@@ -0,0 +1,113 @@
+library(clubSandwich)
+library(robumeta)
+library(metafor)
+library(tidyverse)
+
+source("R/S3_methods.R")
+source("R/helpers.R")
+source("scratchpad/rma_mv.R")  # check this function :D
+source("scratchpad/plot_wildmeta.R")
+source("scratchpad/robu.R") # check this function too :D
+source("scratchpad/Wald_test_wildmeta.R")
+
+
+# metafor -----------------------------------------------------------------
+
+# no intercept
+
+full_model<- rma.mv(yi = d ~ 0 + study_type + hrs + test,
+                     V = V,
+                     random = ~ study_type| study,
+                     data = SATcoaching)
+
+C_mat <- constrain_equal(1:3, coefs = coef(full_model))
+
+# JAMES sometimes this throws convergence issues -
+# so something like safely or something? - and output the # of bootstraps successfully run in waldtest
+boots <- run_cwb(full_model,
+                 C_mat,
+                 R = 99)
+
+plot(boots, fill = "darkred", alpha = 0.6)
+
+
+# need to figure out how to make Wald_test_cwb talk to run_cwb :D
+Wald_test_cwb(full_model,
+              C_mat,
+              R = 99)
+
+# compare to club CR2
+Wald_test(full_model,
+          C_mat,
+          vcov = "CR2")
+
+
+#intercept
+
+full_model <- rma.mv(yi = d ~ study_type + hrs + test,
+                    V = V,
+                    random = ~ study_type| study,
+                    data = SATcoaching)
+
+C_mat <- constrain_zero(2:3, coefs = coef(full_model))
+
+# sometimes this throws convergence issues -
+# so something like safely or something?
+boots <- run_cwb(full_model,
+                 C_mat,
+                 R = 99)
+
+plot(boots, fill = "darkred", alpha = 0.6)
+
+Wald_test_cwb(full_model,
+              C_mat,
+              R = 99)
+
+
+# robumeta ----------------------------------------------------------------
+
+# no intercept
+
+full_model <- robu(d ~ 0 + study_type + hrs + test,
+                   studynum = study,
+                   var.eff.size = V,
+                   small = FALSE,
+                   data = SATcoaching)
+
+C_mat <- constrain_equal(1:3, coefs = full_model$b.r)
+
+
+boots <- run_cwb(full_model,
+                 C_mat,
+                 R = 99)
+
+plot(boots, fill = "darkred", alpha = 0.6)
+
+Wald_test_cwb(full_model,
+              C_mat,
+              R = 99)
+
+Wald_test(full_model,
+          C_mat,
+          vcov = "CR2")
+
+# intercept
+full_model <- robu(d ~ study_type + hrs + test,
+                   studynum = study,
+                   var.eff.size = V,
+                   small = FALSE,
+                   data = SATcoaching)
+
+C_mat <- constrain_zero(2:3, coefs = full_model$b.r)
+
+
+boots <- run_cwb(full_model,
+                 C_mat,
+                 R = 99)
+
+plot(boots, fill = "darkred", alpha = 0.6)
+
+
+Wald_test_cwb(full_model,
+              C_mat,
+              R = 99)
