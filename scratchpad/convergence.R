d876a328557e5b672c17e7ccf89b5ef7880a07cb
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2021-10-26 16:35:45 -0500

    how to handle convergence issues

--- scratchpad/convergence.R
+++ scratchpad/convergence.R
@@ -32,3 +32,16 @@ full_model <- safermamv(rma.mv(yi = d ~ 0 + study_type + hrs + test,
                                data = SATcoaching))
 
 full_model$error
+
+
+SATcoaching <- SATcoaching %>%
+  group_by(study) %>%
+  mutate(esid = row_number()) %>%
+  ungroup()
+
+
+full_model <- rma.mv(yi = d ~ 0 + study_type + hrs + test,
+                     V = V,
+                     random = list(~ study_type| study, ~ study_type|esid),
+                     struct = c("DIAG", "DIAG"),
+                     data = SATcoaching)
