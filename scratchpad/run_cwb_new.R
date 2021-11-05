75ec051c3e46c20af5929ed1126c737bcd225501
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2021-11-05 14:38:33 -0500

    scratch

--- scratchpad/run_cwb_new.R
+++ scratchpad/run_cwb_new.R
@@ -1,147 +1,113 @@
-library(metafor)
-library(robumeta)
 library(clubSandwich)
+library(robumeta)
+library(metafor)
+library(tidyverse)
 
-devtools::load_all()
+# Drop rows with missing predictors
+dat <- SATcoaching[complete.cases(SATcoaching),]
 
-#--------------------------------------------------------------
-# Using robu model
+full_model <- rma.mv(yi = d ~ 0 + study_type + hrs + test,
+                     V = V,
+                     random = ~ study_type| study,
+                     data = dat)
+summary(full_model)
 
-full_model <- robu(d ~ 0 + study_type + hrs + test,
-                   studynum = study,
-                   var.eff.size = V,
-                   small = FALSE,
-                   data = SATcoaching)
+X <- full_model$X
+Cmat <- constrain_equal(1:3, coefs = coef(full_model))
+C_mat <- constrain_equal(1:3, coefs = coef(full_model))
 
-# Not working yet because of some sort of namespace thing
-fitted.values(full_model)
-residuals(full_model)
+# See R/helpers.R for constrain_predictors
+Xnull <- constrain_predictors(X, Cmat)
 
-full_model$fitted.values <- fitted.robu(full_model)
-full_model$residuals <- residuals.robu(full_model)
-fitted.values(full_model)
-residuals(full_model)
+null_auto <- update(full_model, yi = full_model$yi, mods = ~ 0 + Xnull)
+null_auto
 
-# No adjustment
-run_cwb(
-  full_model,
-  cluster = full_model$data.full$study,
-  R = 12
-)
-
-# With CR2 adjustment
-run_cwb(
-  full_model,
-  cluster = full_model$data.full$study,
-  R = 12,
-  adjust = "CR2"
-)
+# Check that null_auto gives the same fitted values as directly specifying the null model
+null_hand <- update(full_model, yi = d ~ hrs + test)
+all.equal(predict(null_auto), predict(null_hand))
 
 
-C_mat <- constrain_equal(1:3, coefs = full_model$b.r)
-constraint_matrix <- C_mat
-R <- 12
-adjust <- "CR0"
-auxiliary_dist <- "Rademacher"
-simplify <- FALSE
-f <- get_boot_F
+# Things get a little bit trickier if there are rows with missing predictors
+full_model <- rma.mv(yi = d ~ 0 + study_type + hrs + test,
+                     V = V,
+                     random = ~ study_type| study,
+                     data = SATcoaching)
+summary(full_model)
 
 
-sapply(bootstraps,
-       FUN = get_boot_F,
-       full_model = full_model,
-       C_mat = C_mat,
-       simplify = simplify)
+X <- full_model$X
+Cmat <- constrain_equal(1:3, coefs = coef(full_model))
+Xnull <- constrain_predictors(X, Cmat)
 
-# added the null model
-null_model <- estimate_null(full_model,
-                            C_mat = constraint_matrix,
-                            R = R)
+update(full_model, yi = full_model$yi, mods = ~ 0 + Xnull)
+# Doesn't work because the random effects in full_model involve all
+# nrow(SATcoaching) observations, but nrow(X) only includes the
+# observations with non-missing values
+
+# This will work for rma.mv() models, I think.
+Xnull_f <- matrix(NA, nrow = nrow(full_model$X.f), ncol = ncol(Xnull))
+Xnull_f[full_model$not.na,] <- Xnull
+null_with_NAs <- update(full_model, yi = full_model$yi.f, mods = ~ 0 + Xnull_f)
+# Check that predictions agree with above
+all.equal(predict(null_auto)$pred, predict(null_with_NAs)$pred)
+
+#all.equal(predict(null_hand)$pred, predict(null_model)$pred)
+
+
+indices <- 2
+
+y_dat <- dplyr::bind_cols(full_model$yi,
+                          full_model$vi,
+                          .name_repair = ~ vctrs::vec_as_names(..., repair = "unique",
+                                                               quiet = TRUE)) %>%
+  dplyr::rename(effect_size = 1,
+                v = 2)
+
+x_dat <- tibble::as_tibble(full_model$X) %>%
+  dplyr::select(-1)
+
+study <- tibble::tibble(study = clubSandwich:::findCluster.rma.mv(full_model))
+
+dat <- dplyr::bind_cols(y_dat, x_dat, study)
+dat$study <- as.character(dat$study)
+
+
+
+null_formula <- paste(rownames(full_model$beta)[ - indices], collapse = " + ")
+null_formula <- stringr::str_replace(null_formula, "intrcpt", "1")
+
+
+
+full_formula <- paste(rownames(full_model$beta), collapse = " + ")
+full_formula <- stringr::str_replace(full_formula, "intrcpt", "1")
+
+
+null_model <- metafor::rma.mv(yi = stats::as.formula(paste("effect_size ~ ", null_formula)),
+                              V = v,
+                              random = ~ 1 | study,
+                              data = dat)
+
+
+# this doesn't work - and I think we need the null model to have the same random effects structure and what not
+null_model <- update(full_model,
+                     yi = stats::as.formula(paste("effect_size ~ ", null_formula)))  # but what if there are other things in the formula
+
+
+null_model <- update(full_model,
+                     yi = stats::as.formula(paste("effect_size ~ ", null_formula)))  # but what if there are other things in the formula
+null_model
+
+
+# it works if i do this but...
+effect_size <- dat$effect_size
+null_model <- update(full_model,
+                     yi = stats::as.formula(paste("effect_size ~ ", null_formula)))  # but what if there are other things in the formula
 
 null_model
 
-null_model$fitted.values <- fitted.robu(null_model)
-null_model$residuals <- residuals.robu(null_model)
 
 
-model$fitted.values <- fitted.robu(model)
-model$residuals <- residuals.robu(model)
-
-boot_stats <- lapply(bootstraps,
-                     f,
-                     full_model = full_model,
-                     C_mat = constraint_matrix)
-
-
-#
-# lapply(bootstraps, f, C_mat)
-
-cluster <- get_cluster(null_model)
-
-run_cwb(
-  null_model,
-  cluster = cluster,
-  R = 12
-)
-
-# Verify wild bootstrap process
-bs <- run_cwb(full_model, cluster = full_model$data.full$study, R = 20)
-
-library(tidyverse)
-bs %>%
-  # back out the auxiliary random variables
-  map_dfc(~ (.x - fitted.values(full_model)) / residuals(full_model)) %>%
-  # check that auxiliaries are constant within cluster
-  mutate(cluster = full_model$data.full$study) %>%
-  group_by(cluster) %>%
-  summarize(
-    across(everything(), ~ diff(range(.x))),
-    .groups = "drop"
-  ) %>%
-  select(-cluster) %>%
-  unlist() %>%
-  sd()
-
-#--------------------------------------------------------------
-# Using rma.mv model
-
-full_model <- rma.mv(yi = d ~ 0 + study_type + hrs + test,
-                      V = V,
-                      random = ~ study_type| study,
-                      data = SATcoaching)
-
-fitted.values(full_model)
-residuals(full_model)
-
-cluster_id <- clubSandwich:::findCluster.rma.mv(full_model)
-
-run_cwb(
-  full_model,
-  cluster = cluster_id,
-  R = 12
-)
-
-# With CR2 adjustment
-run_cwb(
-  full_model,
-  cluster = cluster_id,
-  adjust = "CR2",
-  R = 12
-)
-
-# Verify wild bootstrap process
-bs <- run_cwb(full_model, cluster = cluster_id, R = 12)
-
-bs %>%
-  # back out the auxiliary random variables
-  map_dfc(~ (.x - fitted.values(full_model)) / residuals(full_model)) %>%
-  # check that auxiliaries are constant within cluster
-  mutate(cluster = cluster_id) %>%
-  group_by(cluster) %>%
-  summarize(
-    across(everything(), ~ diff(range(.x))),
-    .groups = "drop"
-  ) %>%
-  select(-cluster) %>%
-  unlist() %>%
-  sd()
+# this works fine
+yi_boot <- rnorm(nrow(dat))
+boot_model <- update(full_model,
+                     yi = yi_boot)
