2ff491f1633d558d63f65e78d606609f30aeecd4
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2022-01-04 14:56:50 -0600

    editing documentation

--- R/Wald_test_wildmeta.R
+++ R/Wald_test_wildmeta.R
@@ -6,7 +6,7 @@
 #' @param constraint_matrix A q X p constraint matrix be tested. Can be specified using constrain_equal, constrain_zero, or constrain_pairwise from the clubSandwich package.
 #' @param R number of bootstrap replications.
 #' @param auxiliary_distribution Character string indicating the auxiliary distribution to be used for cluster wild bootstrapping, with available options: "Rademacher", "Mammen", "Webb six", "uniform", "standard normal". The default is set to "Rademacher." We recommend the Rademacher distribution for models that have at least 10 clusters. For models with less than 10 clusters, we recommend the use of "Webb six" distribution.
-#' @param adjust 	Character string specifying which small-sample adjustment should be used to multiply the residuals by, with available options "CR0", "CR1", "CR2", "CR3", or "CR4". The default is set to CRO, which will multiply the residuals by identity matrices and therefore, wil not add any adjustments to the boostrapping algorithm.
+#' @param adjust 	Character string specifying which small-sample adjustment should be used to multiply the residuals by, with available options "CR0", "CR1", "CR2", "CR3", or "CR4". The default is set to CRO, which will multiply the residuals by identity matrices and therefore, will not add any adjustments to the bootstrapping algorithm.
 #'
 #'
 #' @return A tibble containing the name of the test and the p-value.
