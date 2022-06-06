0f4a50a48c77d81acd44f6812e25431c3f7e2ba9
meghapsimatrix <33493716+meghapsimatrix@users.noreply.github.com>
2022-06-06 15:48:20 -0500

    rma mv

---
+++ scratchpad/future_namespace.R
@@ -0,0 +1,46 @@
+library(future.apply)
+library(clubSandwich)
+library(robumeta)
+library(metafor)
+library(tidyverse)
+
+devtools::load_all()
+
+# a robu model ------------------------------------------------------------
+
+robu_model <- robu(d ~ 0 + study_type + hrs + test,
+                   studynum = study,
+                   var.eff.size = V,
+                   small = FALSE,
+                   data = SATcoaching)
+
+system.time(robu_res <- Wald_test_cwb(full_model = robu_model,
+                                      constraints = constrain_equal(1:3),
+                                      R = 99))
+
+robu_res
+
+
+# metafor -----------------------------------------------------------------
+
+rma_model <- rma.mv(yi = d ~ 0 + study_type + hrs + test,
+                    V = V,
+                    random = ~ study_type | study,
+                    data = SATcoaching)
+
+#this throws error too- list to double?
+system.time(rma_res <- Wald_test_cwb(full_model = rma_model,
+                                     constraints = constrain_equal(1:3),
+                                     R = 99))
+
+rma_res
+
+# breaks when doing parallel? ---------------------------------------------
+plan(multisession)
+
+system.time(rma_res <- Wald_test_cwb(full_model = rma_model,
+                                     constraints = constrain_equal(1:3),
+                                     R = 99))
+
+
+rma_res
